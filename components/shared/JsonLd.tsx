5bc2334d0848431132c063711fe733a87e8f12f8
Hamadine AG MOCTAR <terpoland4@gmail.com>
2026-06-07 12:13:59 +0100

    Refactor JsonLdProps to use unknown type

--- components/shared/JsonLd.tsx
+++ components/shared/JsonLd.tsx
@@ -1,12 +1,7 @@
 interface JsonLdProps {
-  data: Record<string, any>;
+  data: Record<string, unknown>;
 }
 
 export function JsonLd({ data }: JsonLdProps) {
-  return (
-    <script
-      type="application/ld+json"
-      dangerouslySetInnerHTML={{ __html: JSON.stringify(data) }}
-    />
-  );
+  return <script type="application/ld+json" dangerouslySetInnerHTML={{ __html: JSON.stringify(data) }} />;
 }
